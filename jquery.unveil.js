344d2d90eefea25da38a353fffb24b88c6cb5e4a
Michal Zielenkiewicz <michal.zielenkiewicz@allegro.pl>
2014-03-03 15:04:41 +0100

    style background-image with loading class

--- jquery.unveil.js
+++ jquery.unveil.js
@@ -7,51 +7,49 @@
  * Copyright 2013 Luís Almeida
  * https://github.com/luis-almeida
  */
-
-;(function($) {
-
-  $.fn.unveil = function(threshold, callback) {
-
+;
+(function ($) {
+  $.fn.unveil = function (threshold, callback) {
     var $w = $(window),
         th = threshold || 0,
         retina = window.devicePixelRatio > 1,
-        attrib = retina? "data-src-retina" : "data-src",
+        attrib = retina ? "data-src-retina" : "data-src",
         images = this,
         loaded;
-
-    this.one("unveil", function() {
-      var source = this.getAttribute(attrib);
+    this.one("unveil", function () {
+      var source = this.getAttribute(attrib),
+          bgImg,
+          thisElement;
       source = source || this.getAttribute("data-src");
       if (source) {
-        this.setAttribute("src", source);
+        thisElement = $(this);
+        thisElement.addClass('loading');
+        bgImg = new Image();
+        bgImg.onload = function () {
+          thisElement.css('backgroundImage', 'url("' + source + '")');
+          thisElement.removeClass('loading');
+        };
+        bgImg.src = source;
+        bgImg = null;
         if (typeof callback === "function") callback.call(this);
       }
     });
-
     function unveil() {
-      var inview = images.filter(function() {
+      var inview = images.filter(function () {
         var $e = $(this);
         if ($e.is(":hidden")) return;
-
         var wt = $w.scrollTop(),
             wb = wt + $w.height(),
             et = $e.offset().top,
             eb = et + $e.height();
-
         return eb >= wt - th && et <= wb + th;
       });
-
       loaded = inview.trigger("unveil");
       images = images.not(loaded);
     }
-
     $w.scroll(unveil);
     $w.resize(unveil);
-
     unveil();
-
     return this;
-
   };
-
 })(window.jQuery || window.Zepto);
